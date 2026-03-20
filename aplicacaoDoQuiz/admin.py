7f870e8bc52fc89898d45700d5c7f515a03c1cd1
andreiaaa3 <63724731+andreiaaa3@users.noreply.github.com>
2026-03-20 16:31:03 +0000

    Festival model added

--- aplicacaoDoQuiz/admin.py
+++ aplicacaoDoQuiz/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Festival
+
+
+class FestivalAdmin (admin.ModelAdmin):
+    list_display = ("nome",)
+    ordering = ( "nome",)
+    search_fields = ("nome",)
+
+admin.site.register(Festival, Festivaldmin)
--- aplicacaoDoQuiz/models.py
+++ aplicacaoDoQuiz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Festival(models.Model):
+    nome = models.CharField(max_length=100)
+    #morada = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.nome}: {self.morada}'
+
+
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.turma}'
+
+class Bandas(models.Model):
+    ano = models.CharField(max_length=100)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, related_name = "bandas") 
+    festival = models.ManyToManyField(Festival, related_name = "bandas")
+    def __str__(self):
+        return f'{self.ano}: {self.escola}'
